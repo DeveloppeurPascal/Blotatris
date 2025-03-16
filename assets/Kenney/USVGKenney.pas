8cf9b1165e88c44f5d1fa1a3427eb46c75fb3989
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-03-16 18:47:06 +0100

    added copyright and timestamp infos

--- assets/Kenney/USVGKenney.pas
+++ assets/Kenney/USVGKenney.pas
@@ -1,3 +1,35 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Blotatris
+///
+/// Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://blotatris.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Blotatris
+///
+/// ***************************************************************************
+/// File last update : 2025-03-16T17:31:03.157+01:00
+/// Signature : 36e0631177c10707a48d5754addc18ea7682a720
+/// ***************************************************************************
+/// </summary>
+
 unit USVGKenney;
 
 // ****************************************
