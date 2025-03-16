3c5d0f179c631657aa3376dfe898a4db51103bc8
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-03-16 18:47:58 +0100

    added the SVG registration unit

---
+++ src/uSVGBitmapManager_InputPrompts.pas
@@ -0,0 +1,83 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Blotatris
+///
+/// Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://blotatris.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Blotatris
+///
+/// ***************************************************************************
+/// File last update : 2025-03-16T17:45:40.000+01:00
+/// Signature : 0801a52ece3ba40bfb5117a747f44fc6d2494883
+/// ***************************************************************************
+/// </summary>
+
+unit uSVGBitmapManager_InputPrompts;
+
+interface
+
+uses
+  FMX.Graphics,
+  USVGInputPrompts,
+  USVGKenney;
+
+/// <summary>
+/// Returns a bitmap from a SVG image
+/// </summary>
+function getBitmapFromSVG(const Index: TSVGInputPromptsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
+/// <summary>
+/// Returns a bitmap from the SVG from https://kenney.nl
+/// </summary>
+function getBitmapFromSVG(const Index: TSVGKenneyIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
+implementation
+
+uses
+  Olf.Skia.SVGToBitmap;
+
+function getBitmapFromSVG(const Index: TSVGInputPromptsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGInputPrompts.Tag,
+    round(width), round(height), BitmapScale);
+end;
+
+function getBitmapFromSVG(const Index: TSVGKenneyIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGKenney.Tag, round(width),
+    round(height), BitmapScale);
+end;
+
+procedure RegisterSVGImages;
+begin
+  TSVGInputPrompts.Tag := TOlfSVGBitmapList.AddItem(SVGInputPrompts);
+  TSVGKenney.Tag := TOlfSVGBitmapList.AddItem(SVGKenney);
+end;
+
+initialization
+
+RegisterSVGImages;
+
+end.
