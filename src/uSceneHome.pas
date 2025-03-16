31a17a9f1cfc0e82fbb3022f0b56ee1cf3042a34
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-03-16 20:28:07 +0100

    done the home scene

--- src/uSceneHome.pas
+++ src/uSceneHome.pas
@@ -25,8 +25,8 @@
 /// https://github.com/DeveloppeurPascal/Blotatris
 ///
 /// ***************************************************************************
-/// File last update : 2025-03-16T19:01:06.000+01:00
-/// Signature : 3257da42422ec2b48591dcf6830a251654f3c56f
+/// File last update : 2025-03-16T20:15:18.000+01:00
+/// Signature : 9dc801e707554165fa0f42109fa3ab56b3890954
 /// ***************************************************************************
 /// </summary>
 
@@ -46,12 +46,30 @@ uses
   FMX.Forms,
   FMX.Dialogs,
   FMX.StdCtrls,
-  _ScenesAncestor;
+  _ScenesAncestor,
+  _ButtonsAncestor,
+  uTextButton,
+  FMX.Layouts, cGameTitle;
 
 type
   THomeScene = class(T__SceneAncestor)
+    FlowLayout1: TFlowLayout;
+    btnPlay: TTextButton;
+    btnOptions: TTextButton;
+    btnHallOfFame: TTextButton;
+    btnCredits: TTextButton;
+    btnQuit: TTextButton;
+    GameTitle1: TGameTitle;
+    procedure btnCreditsClick(Sender: TObject);
+    procedure btnHallOfFameClick(Sender: TObject);
+    procedure btnOptionsClick(Sender: TObject);
+    procedure btnPlayClick(Sender: TObject);
+    procedure btnQuitClick(Sender: TObject);
   private
   public
+    procedure ShowScene; override;
+    procedure HideScene; override;
+    procedure TranslateTexts(const Language: string); override;
   end;
 
 implementation
@@ -61,7 +79,83 @@ implementation
 uses
   System.Messaging,
   uScene,
-  uConsts;
+  uConsts,
+  uUIElements,
+  uGameData;
+
+{ THomeScene }
+
+procedure THomeScene.btnCreditsClick(Sender: TObject);
+begin
+  TScene.Current := tscenetype.Credits;
+end;
+
+procedure THomeScene.btnHallOfFameClick(Sender: TObject);
+begin
+  TScene.Current := tscenetype.HallOfFame;
+end;
+
+procedure THomeScene.btnOptionsClick(Sender: TObject);
+begin
+  TScene.Current := tscenetype.Options;
+end;
+
+procedure THomeScene.btnPlayClick(Sender: TObject);
+begin
+  tgamedata.DefaultGameData.StartANewGame;
+  TScene.Current := tscenetype.Game;
+end;
+
+procedure THomeScene.btnQuitClick(Sender: TObject);
+begin
+  TScene.Current := tscenetype.Exit;
+end;
+
+procedure THomeScene.HideScene;
+begin
+  inherited;
+  TUIItemsList.Current.RemoveLayout;
+end;
+
+procedure THomeScene.ShowScene;
+begin
+  inherited;
+  TUIItemsList.Current.NewLayout;
+  TUIItemsList.Current.AddControl(btnPlay, nil, nil, btnOptions, nil, true);
+  TUIItemsList.Current.AddControl(btnOptions, btnPlay, nil, btnHallOfFame, nil);
+  TUIItemsList.Current.AddControl(btnHallOfFame, btnOptions, nil,
+    btnCredits, nil);
+{$IF Defined(IOS) or Defined(ANDROID)}
+  btnQuit.Visible := false;
+  TUIItemsList.Current.AddControl(btnCredits, btnHallOfFame, nil, nil, nil);
+{$ELSE}
+  TUIItemsList.Current.AddControl(btnCredits, btnHallOfFame, nil, btnQuit, nil);
+  TUIItemsList.Current.AddControl(btnQuit, btnCredits, nil, nil, nil,
+    false, true);
+{$ENDIF}
+end;
+
+procedure THomeScene.TranslateTexts(const Language: string);
+begin
+  inherited;
+
+  if (Language = 'fr') then
+  begin
+    btnPlay.Text := 'Jouer';
+    btnOptions.Text := 'Options';
+    btnHallOfFame.Text := 'Scores';
+    btnCredits.Text := 'Credits';
+    btnQuit.Text := 'Quitter';
+  end
+  else
+  begin
+    btnPlay.Text := 'Play';
+    btnOptions.Text := 'Options';
+    btnHallOfFame.Text := 'Scores';
+    btnCredits.Text := 'Credits';
+    btnQuit.Text := 'Quit';
+  end;
+end;
 
 initialization
 
@@ -71,11 +165,11 @@ TMessageManager.DefaultManager.SubscribeToMessage(TSceneFactory,
     NewScene: THomeScene;
   begin
     if (Msg is TSceneFactory) and
-      ((Msg as TSceneFactory).SceneType = TSceneType.Home) then
+      ((Msg as TSceneFactory).SceneType = tscenetype.Home) then
     begin
       NewScene := THomeScene.Create(application.mainform);
       NewScene.Parent := application.mainform;
-      tscene.RegisterScene(TSceneType.Home, NewScene);
+      TScene.RegisterScene(tscenetype.Home, NewScene);
     end;
   end);
 
